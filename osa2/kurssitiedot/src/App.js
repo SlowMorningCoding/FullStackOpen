26d7823c86746ca57dc7a891ff2f0d62bf5be4f8
Matti Launiemi <launiemi.matti@gmail.com>
2021-11-04 15:40:25 +0200

    Kurssitiedot 2.4

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,51 +1,83 @@
 function App() {
-  const course = {
-    name: "Half Stack application development",
-    id: 1,
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
     <div>
-      <Course course={course} />
+      <Header name="Web development curriculum" />
+      {courses.map((course) => (
+        <Course course={course} />
+      ))}
     </div>
   );
+}
+
+function Header({ name }) {
+  return <h1>{name}</h1>;
 }
 
 function Course({ course }) {
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
+      <Title name={course.name} />
+      <Content parts={course.parts} />
       <Total parts={course.parts} />
     </div>
   );
 }
 
-function Header({ course }) {
-  return <h1>{course.name}</h1>;
+function Title({ name }) {
+  return <h2>{name}</h2>;
 }
 
-function Content({ course }) {
+
+
+function Content({ parts }) {
   return (
     <div>
-      {course.parts.map((part, i) => {
+      {parts.map((part, i) => {
         return <Part key={i} part={part} />;
       })}
     </div>
@@ -62,7 +94,7 @@ function Part({ part }) {
 
 function Total({ parts }) {
   const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>{"total of " + total + " exercises"}</p>;
+  return <p><b>{"total of " + total + " exercises"}</b></p>;
 }
 
 export default App;
