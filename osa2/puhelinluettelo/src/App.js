3f5081de5d2e1064ecde4f4c0f4458a662e3899a
Matti Launiemi <launiemi.matti@gmail.com>
2021-11-05 08:51:36 +0200

    Puhelinluettelo 2.6

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function App() {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("handleAddPerson", newName);
+    const newPersons = [...persons, { name: newName }];
+    console.log("handleAddPerson", newPersons);
+    setPersons(newPersons);
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
