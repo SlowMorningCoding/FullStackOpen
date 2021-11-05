2a6e0aa29ef290074b5cfb6f3b80afc4577becb0
Matti Launiemi <launiemi.matti@gmail.com>
2021-11-05 15:42:50 +0200

    Puhelinluettelo 2.10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -24,55 +24,85 @@ function App() {
       ...persons,
       { name: newPerson.name, number: newPerson.number },
     ];
-    console.log("handleAddPerson", newPersons);
     setPersons(newPersons);
   };
 
   const handleNameChange = (event) => {
-    console.log(event.target.value);
     setNewPerson({ name: event.target.value, number: newPerson.number });
   };
 
   const handleNumberChange = (event) => {
-    console.log(event.target.value);
     setNewPerson({ name: newPerson.name, number: event.target.value });
   };
 
-  const [numberFilter, setNumberFilter] = useState("");
+  const [personFilter, setPersonFilter] = useState("");
 
-  const handleNumberFilterChange = (event) => {
-    console.log("filter", event.target.value);
-    setNumberFilter(event.target.value);
+  const handlePersonFilterChange = (event) => {
+    setPersonFilter(event.target.value);
   };
 
+  const filteredPersons = persons.filter(
+    (person) => person.name.search(new RegExp(personFilter, "i")) >= 0
+  )
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with:{" "}
-        <input value={numberFilter} onChange={handleNumberFilterChange} />
-      </div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newPerson.name} onChange={handleNameChange} />
-        </div>
-        <div>
-          number:{" "}
-          <input value={newPerson.number} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {persons.filter(person => person.name.search(new RegExp(numberFilter,"i"))>=0).map((person) => (
-          <li key={person.name}>{`${person.name} ${person.number}`} </li>
-        ))}
-      </ul>
+      <Filter value={personFilter} handlePersonFilterChange={handlePersonFilterChange} />
+      <h3>Add a new</h3>
+      <PersonForm
+        submitHandler={addPerson}
+        newPerson={newPerson}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons} />
     </div>
   );
+}
+
+function Filter({ personFilter, handlePersonFilterChange }) {
+  return (
+    <div>
+      filter shown with: <input value={personFilter} onChange={handlePersonFilterChange} />
+    </div>
+  );
+}
+
+function PersonForm({
+  submitHandler,
+  newPerson,
+  handleNameChange,
+  handleNumberChange
+}) {
+  return (
+    <form onSubmit={submitHandler}>
+      <div>
+        name: <input key="name" value={newPerson.name} onChange={handleNameChange} />
+      </div>
+      <div>
+        number:{" "}
+        <input key="number" value={newPerson.number} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+}
+
+function Persons({ persons }) {
+  return (
+    <ul>
+      {persons.map((person,i) => (
+          <Person key={i} person={person}/>
+        ))}
+    </ul>
+  );
+}
+
+function Person({person}) {
+  return <li>{`${person.name} ${person.number}`} </li>;
 }
 
 export default App;
