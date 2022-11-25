27bef596d9b830dd5d328cd5228a376ceed85d4a
Matti Launiemi <launiemi.matti@gmail.com>
2022-11-25 15:43:05 +0200

    updated weather api

--- Maiden_tiedot/src/App.jsx
+++ Maiden_tiedot/src/App.jsx
@@ -1,38 +1,39 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import Filter from "./components/Filter";
-import Countries from "./components/Countries";
-import Country from "./components/Country";
-import Weather from "./components/Weather";
-import axios from "axios";
+import Filter from './components/Filter';
+import Countries from './components/Countries';
+import Country from './components/Country';
+import Weather from './components/Weather';
+import axios from 'axios';
 
 function App() {
   // Countries
-  const [countries, setCountries] = useState([]);
-  const [countryFilter, setCountryFilter] = useState("");
-  const [selectedCapital, setSelectedCapital] = useState("");
-  const [weather, setWeather] = useState({});
+  const [countryData, setCountryData] = useState([]);
+  const [countryFilter, setCountryFilter] = useState('');
+  const [selectedCountry, setSelectedCountry] = useState({});
+  const [weatherData, setWeatherData] = useState({});
 
   useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then((response) => {
-      setCountries(response.data);
+    console.log('GET countries data');
+    axios.get('https://restcountries.com/v3.1/all').then((response) => {
+      setCountryData(response.data);
     });
   }, []);
 
   useEffect(() => {
-    if (selectedCapital.length) {
-      /*axios
-        .get(`https://restcountries.com/v3.1/capital/${selectedCapital}`)
-        .then((response) => {
-          console.log("Weather response:", response);
-          if (response.data.length === 1) {
-            setWeather(response.data[0]);
-          }
-        });
-      */
-      setWeather({ capital: selectedCapital });
+    console.log('selectedCountry', selectedCountry);
+    if (Object.prototype.hasOwnProperty.call(selectedCountry, 'latlng')) {
+      console.log('GET wather data');
+      axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${selectedCountry.latlng[0]}&longitude=${selectedCountry.latlng[1]}&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`)
+      .then((response) => {
+        console.log("Weather response:", response.data);
+        if (response.data.length === 1) {
+          setWeatherData(response.data[0]);
+        }
+      });
     }
-  }, [selectedCapital]);
+    console.log("weatherData", weatherData)
+  }, [selectedCountry]);
 
   const filterOnChange = (event) => {
     setCountryFilter(event.target.value);
@@ -42,27 +43,20 @@ function App() {
     setCountryFilter(country);
   };
 
-  const countriesToShow = countries.filter(
-    (country) => country.name.common.search(new RegExp(countryFilter, "i")) >= 0
+  const countriesToShow = countryData.filter(
+    (country) => country.name.common.search(new RegExp(countryFilter, 'i')) >= 0,
   );
-  console.log("countriesToShow:", countriesToShow.length);
-  if (
-    countriesToShow.length === 1 &&
-    selectedCapital !== countriesToShow[0].capital
-  ) {
-    console.log("setSelectedCapital", countriesToShow[0].capital);
-    setSelectedCapital(countriesToShow[0].capital);
+  console.log('countriesToShow:', countriesToShow.length);
+  if (countriesToShow.length === 1 && selectedCountry !== countriesToShow[0]) {
+    console.log('setSelectedCountry', countriesToShow[0]);
+    setSelectedCountry(countriesToShow[0]);
   }
 
   return (
     <div>
       <h2>Country Search</h2>
       <Filter value={countryFilter} changeHandler={filterOnChange} />
-      <CountriesToShow
-        countries={countriesToShow}
-        countriesOnClick={countriesOnClick}
-        weather={weather}
-      />
+      <CountriesToShow countries={countriesToShow} countriesOnClick={countriesOnClick} weather={weatherData} />
     </div>
   );
 }
@@ -76,9 +70,7 @@ function CountriesToShow({ countries, countriesOnClick, weather }) {
       </div>
     );
   } else if (countries.length <= 10) {
-    return (
-      <Countries countries={countries} countriesOnClick={countriesOnClick} />
-    );
+    return <Countries countries={countries} countriesOnClick={countriesOnClick} />;
   }
   return <p>Too many matches, specify another filter</p>;
 }
--- Maiden_tiedot/src/components/Weather.jsx
+++ Maiden_tiedot/src/components/Weather.jsx
@@ -1,5 +1,25 @@
 import PropTypes from 'prop-types';
 
+/**
+ * WMO Weather interpretation codes (WW)
+ * Code	    Description                                       icon
+ * 0	      Clear sky                                         01d.png clear sky
+ * 1        Mainly clear                                      04d.png broken clouds
+ * 2        Partly cloudy                                     03d.png scattered clouds
+ * 3        Overcast                                          02d.png few clouds
+ * 45,48	  Fog and depositing rime fog                       50d.png mist
+ * 51,53,55	Drizzle: Light, moderate, and dense intensity     09d.png shower rain
+ * 56,57	  Freezing Drizzle: Light and dense intensity       09d.png shower rain
+ * 61,63,65	Rain: Slight, moderate and heavy intensity        10d.png rain
+ * 66,67	  Freezing Rain: Light and heavy intensity          10d.png rain
+ * 71,73,75	Snow fall: Slight, moderate, and heavy intensity  13d.png snow
+ * 77	      Snow grains                                       13d.png snow
+ * 80,81,82	Rain showers: Slight, moderate, and violent       10d.png rain
+ * 85,86	  Snow showers slight and heavy                     13d.png snow
+ * 95       Thunderstorm: Slight or moderate                  11d.png thunderstorm
+ * 96,99    Thunderstorm with slight and heavy hail           11d.png thunderstorm
+ */
+
 function Weather({ weather }) {
   return (
     <div>
